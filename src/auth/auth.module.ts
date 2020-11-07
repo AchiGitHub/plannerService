ec27f4830b8e3aa569e146696ceb2ac946d0dbd1
Achintha Ikiriwatte <achinthaikiriwatte@Achinthas-MacBook-Pro.local>
2020-11-07 23:21:39 +0530

    implemented jwt generation on login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
 import { LocalStratergy } from './local.stratergy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStratergy]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' }
+    }),
+  ],
+  providers: [AuthService, LocalStratergy],
+  exports: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
