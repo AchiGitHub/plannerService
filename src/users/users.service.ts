eb08b88f849444e1e4fbd84a5e790e0c0204cc33
Achintha Ikiriwatte <achinthaikiriwatte@Achinthas-MacBook-Pro.local>
2020-11-12 17:14:27 +0530

    implemented JWT stratergy

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 import { User } from './interfaces/user.interface';
 import { UserRepository } from './users.repository';
 
@@ -12,15 +14,24 @@ export class UsersService {
     constructor(
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
-    ) {}
+        private jwtService: JwtService
+    ) { }
 
     signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.signUp(authCredentialsDto);
     }
 
-    async signIn(authCredentialsDto: AuthCredentialsDto) {
-        const result = await this.userRepository.validateUserPassword(authCredentialsDto);
-        console.log(result);
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
+        const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+
+        if (!username) {
+            throw new UnauthorizedException('Invalid Credentials')
+        }
+
+        const payload: JwtPayload = { username, role: "client" };
+        const accessToken = await this.jwtService.sign(payload);
+
+        return { accessToken };
     }
 
 }
