7c85fab14566c5aec177e421d589359c0b466eaa
Achintha Ikiriwatte <achinthaikiriwatte@Achinthas-MacBook-Pro.local>
2020-11-13 12:57:19 +0530

    added swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Planners Service')
+    .setDescription('Services belonging to the clients/planners/projects')
+    .setVersion('1.0')
+    .addTag('PlanApp')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
